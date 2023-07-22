512426d8b695bbf881e2d5387adb741d26d99dd0
Pratishtha Verma <pratishthaverma2000@gmail.com>
2023-07-23 00:02:05 +0530

    done changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var typed = new typed(".text", {
-    Strings:["Full-Stack Developer" , "Front-End Developer" , "Back-end Developer" ],
+var typed = new Typed(".text", {
+    strings:["Full-Stack Developer" , "Front-End Developer" , "Back-end Developer" ],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-})
+});
