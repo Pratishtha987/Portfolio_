5d47403bda8dd488eccd4a925de18868c6146076
Pratishtha Verma <pratishthaverma2000@gmail.com>
2023-07-22 14:04:50 +0530

    Added html css

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings:["Full-Stack Developer" , "Front-End Developer" , "Back-end Developer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
